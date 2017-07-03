5175f214a8ae917f30e61450318bdc455453c880
vikramriyer <vikram.iyer09@gmail.com>
2017-07-03 22:12:37 +0530

    prac, focus: adding code to check pair sum using hash table

--- pair_sum.py
+++ pair_sum.py
@@ -7,21 +7,40 @@ def get_pair_simple(arr, sum):
 			if i + j == sum and arr.index(i) != arr.index(j):
 				return i, j
 
+def get_pair_hash(arr, sum):
+	'''
+	Algo:
+	  if sum - elem of array in hash and hash[elem] == 1
+	  	then return the elem and sum - elem i.e. the pair
+	  else
+		iterate
+	'''
+	#initialize hashmap
+
+	elem_hash = [0]*100000
+
+	for i in range(0, len(arr)):
+		remainder = sum - arr[i]
+		if remainder >= 0 and elem_hash[remainder] == 1:
+			return remainder, arr[i]
+		elem_hash[arr[i]] = 1
+
 # Driver code to run the program
 sum = 22
 arr = [5, 10, 12, 25, 6]
 a, b = get_pair_simple(arr, sum)
 print a, b
+a, b = get_pair_hash(arr, sum)
+print a, b
 
 
 def get_pair_bst():
-	'''Algo: store the input in a bst, searching will take logn i.e. for each elem logn search time
-	Hence, time complexity = O(nlogn)'''
+	'''
+		Algo: store the input in a bst, searching will take logn i.e. for each elem logn search time
+		Hence, time complexity = O(nlogn)
+	'''
 	pass
 
-def get_pair_hash():
-	''''''
-	pass
 
 '''
 input: 5 10 12 25 6
