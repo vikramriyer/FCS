ca428936ca76506106fe293a0e047a6378006aa4
vikramriyer <vikram.iyer09@gmail.com>
2017-07-03 21:21:59 +0530

    rotate an image by 90 degrees

--- rotate_image.c
+++ rotate_image.c
@@ -1,1 +1,29 @@
 #include<stdio.h>
+
+/*
+Algo:
+- do a transpose of the matrix
+- swap row 1 with row 3
+*/
+
+int transpose(int);
+int swap_rows(int);
+
+void main(){
+	int arr[3][3] = {0,1,2,3,4,5,6,7,8}; // array initialization
+	int n = sizeof(arr)/sizeof(arr[0][0]); // length of 2D array
+	printf("%d\n", n); 
+
+	int arrr = transpose(arr);
+	//arr = swap_rows(arr);
+}
+
+int transpose(int a){
+	int t_a[3][3] = {1,2,3,4,5,6,7,8,9};
+	return t_a;
+}
+
+int swap_rows(int a){
+	int s_a[3][3] = {1,2,3,4,5,6,7,8,9};
+	return s_a;
+}
--- rotate_image.py
+++ rotate_image.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python
 
+init_list = [[1,2,3],[4,5,6],[7,8,9]]
+
+def rotate_matrix(matrix):
+	'''Algo: 
+	   	First: take transpose of the matrix
+		Second: interchange row 0 and row 2
+	'''
+
+	matrix = zip(*matrix)
+	return swap_rows(matrix)
+
+def swap_rows(matrix):
+	print matrix	
+	row0, row2 = matrix[0], matrix[2]
+
+	matrix[0] = row2
+	matrix[2] = row0
+
+	return matrix 
+
+final_matrix = rotate_matrix(init_list)
+print final_matrix
+
