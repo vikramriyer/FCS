393d2c5e4024abf421b0560360d1768928930a70
vikramriyer <vikram.iyer09@gmail.com>
2017-07-05 20:21:19 +0530

    prac: in progress

---
+++ total_connected_components_in_a_graph.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+graph =  [[0, 1, 0, 1, 1],
+          [0, 0, 1, 0, 0],
+          [0, 0, 0, 1, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]]
+
+'''
+Algo:
+1. initially mark all the vertices as not visited
+2. 
+'''
